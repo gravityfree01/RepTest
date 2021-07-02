ec53fddadc3e1fa796283901ad17f8488cd79dd4
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-07-03 01:57:22 +0900

    Test_SS_Scenes - player,Feed,Enemy 스프라이트생성, TestClickEventSS.cs 게임 좌우버튼 스크립트 생성중

---
+++ AnimalGame/Assets/Scripts/TestClickEventSS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 2021-07-03 박성수 작성
+// 게임 좌우 버튼 생성중
+public class TestClickEventSS : MonoBehaviour{
+    public GameObject btn1;
+    public GameObject btn2;
+    
+    public void OnClickButton1()
+    {
+       
+
+        btn2.GetComponent<Button>().interactable = true;
+        btn1.GetComponent<Button>().interactable = false;
+    }
+
+    public void OnClickButton2()
+    {
+        btn1.GetComponent<Button>().interactable = true;
+        btn2.GetComponent<Button>().interactable = false;
+    }
+
+}
