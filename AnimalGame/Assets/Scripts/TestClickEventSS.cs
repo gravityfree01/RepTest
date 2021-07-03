0a1b3e7d55c140c3bb1838339e71ea4d30e8c20b
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-07-03 09:56:05 +0900

    스크립트 TestClickEventSS - 게임 좌우 컨트롤 버튼 작업중

--- AnimalGame/Assets/Scripts/TestClickEventSS.cs
+++ AnimalGame/Assets/Scripts/TestClickEventSS.cs
@@ -9,18 +9,56 @@ public class TestClickEventSS : MonoBehaviour{
     public GameObject btn1;
     public GameObject btn2;
     
+    private GameObject obj;
+
+    private void Start(){
+        if (FindObject())
+        {
+            Debug.Log("Find Object");
+        }
+        else
+        {
+            Debug.Log("Don't");
+        }
+    }
+
+    private bool FindObject()
+    {
+        if (obj == null) { 
+       obj =GameObject.Find("Player").gameObject;
+            return true;
+        }
+        return false;
+    }
+
+
+
     public void OnClickButton1()
     {
        
 
         btn2.GetComponent<Button>().interactable = true;
         btn1.GetComponent<Button>().interactable = false;
+        btn2.GetComponent<Image>().color = Color.red;
+        btn1.GetComponent<Image>().color = Color.white;
+
+        btn1.gameObject.SetActive(false);
+
+        if (!obj.gameObject.activeSelf) return;
+        obj.gameObject.SetActive(false);
     }
 
     public void OnClickButton2()
     {
         btn1.GetComponent<Button>().interactable = true;
         btn2.GetComponent<Button>().interactable = false;
+        btn1.GetComponent<Image>().color = Color.red;
+        btn2.GetComponent<Image>().color = Color.white;
+
+        btn2.gameObject.SetActive(false);
+
+        if (obj.gameObject.activeSelf) return;
+        obj.gameObject.SetActive(true);
     }
 
 }
