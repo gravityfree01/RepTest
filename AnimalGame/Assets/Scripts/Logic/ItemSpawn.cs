67260892094f8a18073b0b7227bd3e7f96965716
sH <sh1222j@gmail.com>
2021-07-12 00:27:35 +0900

    ItemSpawn.cs -  아이템 소환 스크립트 작성 완료

--- AnimalGame/Assets/Scripts/Logic/ItemSpawn.cs
+++ AnimalGame/Assets/Scripts/Logic/ItemSpawn.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 /*
  * @class ItemSpawn
- * @desc 아이템 기능 정보를 담는 클래스
- * @author  정성호
+ * @desc  아이템 소환 클래스
+ * @author 정성호
  * @date  2021-07-11
  */
 
@@ -15,7 +15,10 @@ public class ItemSpawn : MonoBehaviour
     private MemoryPool memoryPool;           // 메모리 풀
     private GameObject[] itemArray;  // 메모리 풀과 연동하여 사용할 Feed 배열
     private int itemMaxPool = 1;
+
+    private TextMesh countdownText;
     private SpriteRenderer spriteRenderer;
+
     private float time = 0f;
 
     void Start()
@@ -25,6 +28,7 @@ public class ItemSpawn : MonoBehaviour
         itemArray = new GameObject[itemMaxPool];
 
         spriteRenderer = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+        countdownText = GetComponent<TextMesh>();
     }
 
     void Update()
@@ -75,10 +79,16 @@ public class ItemSpawn : MonoBehaviour
             spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
             Debug.Log(Item.isEnabled + " 활성화됨.");
 
-            // 이 부분에 무적이 됐을 때 어떻게 처리할 것인지 추가
-
             time += Time.deltaTime;
-            if (time >= 5f) // 무적 시간 종료되면
+
+            // 무적이 됐을 때
+            if (time < item.durationTime)
+            {
+
+            }
+
+            // 무적 시간이 종료되면
+            if (time > item.durationTime)
             {
                 spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
                 Item.isEnabled = false;
