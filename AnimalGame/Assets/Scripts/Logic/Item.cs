ed5bd3446361f67ee3035e737c8f17816a123d49
sH <sh1222j@gmail.com>
2021-07-11 18:48:53 +0900

    Item.cs / ItemSpawn.cs / JSH_Item.prefab / JSH_Scene_Feed.unity / 5초 무적 스크립트작성 및 씬에 적용(에셋은 테스트로 시계로 함) (장애물에 맞으면 5초 반투명되면서 5초 무적상태가 됨)

---
+++ AnimalGame/Assets/Scripts/Logic/Item.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+/*
+ * @class Item
+ * @desc 아이템 정보를 담는 클래스
+ * @author  정성호
+ * @date  2021-07-11
+ */
+
+public class Item : MonoBehaviour
+{
+    public GameObject itemObject; // 아이템 오브젝트
+    public Transform itemLocation;// 아이템 생성 위치
+    public float durationTime;    // 지속 시간  
+    public static bool isEnabled;        // 아이템 활성화 상태
+  
+    private float DestroyPosY = -5f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime);
+
+        if (transform.position.y <= DestroyPosY)
+            GetComponent<Collider2D>().enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<Collider2D>().enabled = false;
+            isEnabled = true;
+            Debug.Log("아이템을 섭취함. " + isEnabled);
+        }
+        // 획득시 효과음 발생
+    }
+}
---
+++ AnimalGame/Assets/Scripts/Logic/ItemSpawn.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/*
+ * @class ItemSpawn
+ * @desc 아이템 기능 정보를 담는 클래스
+ * @author  정성호
+ * @date  2021-07-11
+ */
+
+public class ItemSpawn : MonoBehaviour
+{
+    public Item item;
+
+    private MemoryPool memoryPool;           // 메모리 풀
+    private GameObject[] itemArray;  // 메모리 풀과 연동하여 사용할 Feed 배열
+    private int itemMaxPool = 1;
+    private SpriteRenderer spriteRenderer;
+    private float time = 0f;
+
+    void Start()
+    {
+        memoryPool = new MemoryPool();
+        memoryPool.Create(item.itemObject, itemMaxPool);
+        itemArray = new GameObject[itemMaxPool];
+
+        spriteRenderer = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        SpawnItem();
+        GodMode();
+    }
+
+    private void SpawnItem()
+    {
+        StartCoroutine(ItemCycleControl());
+
+        for (int i = 0; i < itemMaxPool; i++)
+        {
+            if (itemArray[i] == null)
+            {
+                itemArray[i] = memoryPool.NewItem();
+
+                Vector2 vector = Vector2.zero;
+                vector.x = Random.Range(-2f, 2f);
+                vector.y = 5f;
+
+                item.itemLocation.transform.position = vector;
+                itemArray[i].transform.position = item.itemLocation.transform.position;
+
+                break;
+            }
+        }
+
+        for (int i = 0; i < itemMaxPool; i++)
+        {
+            if (itemArray[i])
+            {
+                if (itemArray[i].GetComponent<Collider2D>().enabled == false)
+                {
+                    itemArray[i].GetComponent<Collider2D>().enabled = true;
+                    memoryPool.RemoveItem(itemArray[i]);
+                    itemArray[i] = null;
+                }
+            }
+        }
+    }
+
+    private void GodMode()
+    {
+        if (Item.isEnabled) // 무적 아이템을 먹으면
+        {
+            spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
+            Debug.Log(Item.isEnabled + " 활성화됨.");
+
+            // 이 부분에 무적이 됐을 때 어떻게 처리할 것인지 추가
+
+            time += Time.deltaTime;
+            if (time >= 5f) // 무적 시간 종료되면
+            {
+                spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+                Item.isEnabled = false;
+                time = 0;
+            }
+        }
+    }
+
+    IEnumerator ItemCycleControl()
+    {
+        yield return new WaitForSeconds(3f);
+    }
+}
