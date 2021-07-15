4b894bb2893466856d317b35d924d12c7e51939d
sH <sh1222j@gmail.com>
2021-07-15 18:17:59 +0900

    Enemy.cs / IngameScene.unity - 에너미 작업 완료 및 인게임씬 에너미, 목숨 도입

--- AnimalGame/Assets/Scripts/Logic/Enemy.cs
+++ AnimalGame/Assets/Scripts/Logic/Enemy.cs
@@ -1,58 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-/**
- * @class Enemy                         // 클래스 이름
- * @desc 적 클래스정보를 담는 클래스      //클래스 설명
- * @author  양동건                       // 작성자
- * @date  2021-07-09        //클래스 작성일자
+/*
+ * @class Enemy
+ * @desc  적군 클래스
+ * @author 정성호
+ * @date  2021-07-15
  */
- 
-public class Enemy : ObjectParent
+
+public class Enemy : MonoBehaviour
 {
-    /**
-    * @author  박성수                  
-    * @date  2021-07-08        
-    * @sumary 
-    *    Enemy tag를 이용한 스크립작성
-    *    Enemy가 떨어지는 속도 작성
-    */
-    string playerTag = "Player";
+    private Life life;
 
-    void Update(){
-        Destroy(this.gameObject);
-        if (this.transform.position.y > 0)
-        {
-            this.transform.position -= new Vector3(0.15f * Time.fixedDeltaTime, 0, 0);
-        }
+    public GameObject enemyObject;
+    public Transform enemyLocation;
+
+    private float DestroyPosY = -3f;
+
+    private void Start()
+    {
+        life = GameObject.Find("LifeText").GetComponent<Life>();
     }
 
-    /**
-     * @author  박성수                       // 작성자
-     * @date  2021-07-08        //클래스 작성일자
-     * @sumary Enemy 가 Player와 충돌시 사라지는 코드작성     // 작성한 내용
-     */
-    private void OnCollisionEnter2D(Collision2D col)
+    void Update()
     {
-        if(col.gameObject.tag == playerTag )
-        {
-            Destroy(this.gameObject);
-            //Destroy(col.gameObject);
-        }
+        transform.Translate(Vector2.down * Time.deltaTime);
+
+        if (transform.position.y <= DestroyPosY)
+            GetComponent<Collider2D>().enabled = false;
     }
 
-    /**
-     * @function foo(int a, GameObject obj)
-     * @param1 {int} a 정수값들어옴
-     * @param2 {GameObject} obj 게임오브젝트 객체 추가
-     * @author  박성수                     
-     * @date  2021-07-08        
-     * @sumary Enemy 가 Player와 충돌시 사라지는 코드작성    
-     */
-    private void foo(int a, GameObject obj)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
-
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<Collider2D>().enabled = false;
+            life.LifeDecrease();
+        }
     }
 }
