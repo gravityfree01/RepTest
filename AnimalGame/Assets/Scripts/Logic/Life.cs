89530b872af2c0bb0cd83ced7f65de89980fad7c
sH <sh1222j@gmail.com>
2021-07-15 18:16:03 +0900

    Life.cs - 플레이어 목숨 작업 완료(3개 목숨)

---
+++ AnimalGame/Assets/Scripts/Logic/Life.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * @class Life
+ * @desc  목숨 클래스
+ * @author 정성호
+ * @date  2021-07-15
+ */
+
+public class Life : MonoBehaviour
+{
+    public int lifeCount = 3;
+    public Text lifeText;
+
+    void Update()
+    {
+        lifeText.text = lifeCount.ToString();
+        Debug.Log("라이프 개수: " + lifeCount);
+    }
+
+    public void LifeDecrease()
+    {
+        lifeCount--;
+    }
+}
