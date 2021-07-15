0e60a106ee3a814183990546927518d848dcb685
sH <sh1222j@gmail.com>
2021-07-15 13:32:19 +0900

    Revert "Player.cs - 좌우 투명버튼으로 터치구현 완료(안드로이드 터치 테스트 완료)"
    This reverts commit 67198c39ea502f70af2680c3901ae863e6cd2a1c.

--- AnimalGame/Assets/Scripts/Logic/Player.cs
+++ AnimalGame/Assets/Scripts/Logic/Player.cs
@@ -9,41 +9,68 @@ using UnityEngine;
  * @date  2021-07-09 */
 
 public class Player : MonoBehaviour
-
-
 {
-    public float moveSpeed = 1.0f; // 이동속도
-    public Vector3 moveDirection = Vector3.zero; // 이동방향
+    public float moveSpeed = 1.0f;
+    public Vector3 moveDirection = Vector3.zero;
 
     void Update()
     {
-        int width = Screen.width;
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (width / 2 > touch.position.x)
-            {
-                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-                    transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-            }
-
-            if (width / 2 < touch.position.x)
-            {
-                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
-                    transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-            }
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))  // → 방향키를 누를 때
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.LeftArrow))  // ← 방향키를 누를 때
-            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-
-        // 플레이어 캐릭터가 화면 범위 바깥으로 나가지 못하도록함
+        float x = Input.GetAxisRaw("Horizontal");
+        moveDirection = new Vector3(x, 0, 0);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
         if (pos.x < 0f) pos.x = 0f;
         if (pos.x > 1f) pos.x = 1f;
         transform.position = Camera.main.ViewportToWorldPoint(pos);
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(x, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.position = curPos + nextPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)    {
+        if (collision.gameObject.tag == "Player")        {
+            collision.gameObject.SetActive(false);
+        }
     }
 }
+
+			 
+															   
+ 
+
+			  
+							
+
+											
+
+
+						
+	 
+ 
+
+					  
+					   
+
+ 
+
+						  
+						
+
+ 
+
+				   
+												 
+																		  
+								  
+									 
+
+							
+ 
+ 
+  
+
+
+
+
+
+
