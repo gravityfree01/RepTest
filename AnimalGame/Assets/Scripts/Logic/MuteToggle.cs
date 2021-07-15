8dcf4859da3d550c44dc1fe2ccdb94ac7fc6c885
sH <sh1222j@gmail.com>
2021-07-15 16:29:55 +0900

    StartScene.unity - 사운드 버튼 토글 스위치 구현 완료(버튼전환까진 됬으나 실제 사운드 테스트는 못해봄)

---
+++ AnimalGame/Assets/Scripts/Logic/MuteToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class MuteToggle : MonoBehaviour
+{
+    Toggle myToggle;
+
+    void Start()
+    {
+        myToggle = GetComponent<Toggle>();
+        if(AudioListener.volume == 0)
+        {
+            myToggle.isOn = false;
+        }
+    }
+
+    public void ToggleAuioOnValueChange(bool audioIn)
+    {
+        if(audioIn)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
