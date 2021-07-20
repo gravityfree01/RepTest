bebdeac7b0c0082630240c09ab87bcc3dbf348da
sH <sh1222j@gmail.com>
2021-07-20 20:43:34 +0900

    GameOver.unity -> GameOverScene.unity 이름 수정 Resources\UI\game over background.png / \help.png - 게임오버 배경사진 및 헬프사진파일, 게임오버로고 추가 GameOverScene.unity - 게임오버씬 제작 완료.(버튼 작동 테스트 결과 정상 작동 확인) StartScene.unity - BGM 추가 및 무음스위치 정상 작동 확인 UIController.cs - 씬이동 함수 추가(스타트씬, 인게임씬, 헬프씬), 종료 함수 추가

--- AnimalGame/Assets/Scripts/Logic/UIController.cs
+++ AnimalGame/Assets/Scripts/Logic/UIController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -31,8 +31,24 @@ public class UIController : MonoBehaviour{
         }
     }
 
+
+
+    // 스타트씬
+    public void StartScene(){
+        SceneManager.LoadScene(0);
+    }
+
+    // 인게임씬
+    public void IngameScene(){
+        SceneManager.LoadScene(1);
+    }
+    // 게임오버
     public void FailScene(){
         SceneManager.LoadScene(2);
+    }
+    // 헬프씬
+    public void HelpScene(){
+        SceneManager.LoadScene(3);
     }
 
     public void SetCountDownText(int number){
@@ -50,4 +66,12 @@ public class UIController : MonoBehaviour{
         if (this.countDownText.gameObject.activeSelf) this.countDownText.gameObject.SetActive(false);
     }
 
+    // 종료 버튼
+    public void ExitGame(){
+#if UNITY_ANDROID
+        Application.Quit();
+#elif UNITY_EDITOR
+        Debug.Log("Game Quit");
+#endif
+    }
 }
